94acd535eebdbe4af22f90e5491103381b3e3e63
Gauransh <mayankkumar37739@gmail.com>
2026-03-13 00:00:32 +0530

    Courselist,searchbar

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -3,11 +3,13 @@ import Hero from '../../components/student/Hero'
 import Features from '../../components/student/Features'
 import CoursesSection from '../../components/student/CourseSection'
 import LearningPath from '../../components/student/LearningPath'
+import SearchBar from '../../components/student/SearchBar'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center '>
       <Hero />
+      
       <Features />
       <CoursesSection />
       <LearningPath />
