8dd16b7aed577d77b041d29d9d064ec5e67bfa08
Kartikey Singh <kartikey.ks03@gmail.com>
2026-04-19 19:38:22 +0530

    server file update

--- Server/server.js
+++ Server/server.js
@@ -4,8 +4,8 @@ import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 // import connectCloudinary from './configs/cloudinary.js'
 // import userRouter from './routes/userRoutes.js'
-// import { clerkMiddleware } from '@clerk/express'
-// import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
+import { clerkMiddleware } from '@clerk/express'
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
 // import educatorRouter from './routes/educatorRoutes.js'
 // import courseRouter from './routes/courseRoute.js'
 
@@ -22,7 +22,7 @@ app.use(cors())
 
 // Routes
 app.get('/', (req, res) => res.send("API Working"))
-// app.post('/clerk', express.json() , clerkWebhooks)
+app.post('/clerk', express.json() , clerkWebhooks)
 // app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 // app.use('/api/educator', express.json(), educatorRouter)
 // app.use('/api/course', express.json(), courseRouter)
